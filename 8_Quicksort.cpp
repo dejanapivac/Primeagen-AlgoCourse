0c3c3cc12b2e32758087ad4da2ba33c2f13eb88d
Dejana Pivac <dejanapivac@gmail.com>
2023-12-06 15:14:31 +0100

    quicksort

--- 8_Quicksort.cpp
+++ 8_Quicksort.cpp
@@ -3,11 +3,7 @@
 
 using namespace std;
 
-void qs(vector<int> arr, int lo, int hi) {
-
-}
-
-int partition(vector<int> arr, int lo, int hi) { // return pivot index
+int partition(vector<int>& arr, int lo, int hi) { // return pivot index
     int pivot = arr[hi];
     int idx = lo - 1;
 
@@ -21,9 +17,34 @@ int partition(vector<int> arr, int lo, int hi) { // return pivot index
     }
 
     idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+void qs(vector<int>& arr, int lo, int hi) {
+    if(lo >= hi) {
+        return;
+    }
+
+    int pivotIdx = partition(arr, lo, hi);
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
 }
 
 int main() {
+    vector<int> arr = {12, 4, 5, 6, 7, 3, 1, 15};
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    qs(arr, 0, arr.size() - 1);
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
 
     return 0;
 }
